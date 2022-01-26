de4839c1ca6402b7b436927a79b858377dd862d3
ElCabronw <lucasfuziyama@gmail.com>
2022-01-26 20:01:14 -0300

    Add RepositoryInterface and Created ProductVO

---
+++ GeekShopping.ProductAPI/Data/ValueObjects/ProductVO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace GeekShopping.ProductAPI.Data.ValueObjects
+{
+	public class ProductVO
+	{
+		public ProductVO()
+		{
+		}
+		public long Id { get; set; }
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+		public string Description { get; set; }
+		public long CategoryId { get; set; }
+		public string ImageURL { get; set; }
+
+	}
+}
+
