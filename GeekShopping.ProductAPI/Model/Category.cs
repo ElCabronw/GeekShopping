5428ef26c952bd91d2e81eab0407dd5502d2ac41
ElCabronw <lucasfuziyama@gmail.com>
2022-01-26 19:48:26 -0300

    First Commit

---
+++ GeekShopping.ProductAPI/Model/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GeekShopping.ProductAPI.Model.Base;
+
+namespace GeekShopping.ProductAPI.Model
+{
+	[Table("category")]
+	public class Category : BaseEntity
+	{
+		[Column("name")]
+		[Required]
+		[StringLength(50)]
+        public string Name { get; set; }
+
+		[Column("description")]
+		[StringLength(500)]
+        public string Description { get; set; }
+
+		public ICollection<Product> Products { get; set; }
+
+	}
+}
+
---
+++ GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,51 @@
+﻿using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connection = builder.Configuration.GetConnectionString("MySQLConnectionString");
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 28))));
+
+
+
+
+//builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+var scope = app.Services.CreateScope();
+
+//var dbInitializer = scope.ServiceProvider.GetService<IDbInitializer>();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseIdentityServer();
+
+app.UseAuthorization();
+
+
+//dbInitializer.Initialize();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
